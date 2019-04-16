4b59666054cf5511114ed9c7768f8ea30c105364
Mrinal Kanti Ghosh <mrinal2196ghosh@gmail.com>
2019-04-16 16:38:54 +0530

    [Praveen/Mrinal] add map functionality based on current location

--- lib/widget/maps.dart
+++ lib/widget/maps.dart
@@ -1,5 +1,6 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MyMaps extends StatefulWidget {
   @override
@@ -9,19 +10,36 @@ class MyMaps extends StatefulWidget {
 
 class MyMapsState extends State<MyMaps> {
   GoogleMapController mapController;
-  CameraPosition cameraPosition = new CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746
-  );
+
+  var currentLocation;
+
+  void initState(){
+    super.initState();
+    Geolocator().getCurrentPosition().then((currentLoc){
+      setState(() {
+        currentLocation =  currentLoc;
+      });
+    });
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      initialCameraPosition: cameraPosition,
-      compassEnabled: true,
-      onMapCreated: (controller) {
-        mapController = controller;
-      },
-    );
+    if(currentLocation != null){
+      return GoogleMap(
+        initialCameraPosition: new CameraPosition(
+            target: LatLng(currentLocation.latitude, currentLocation.longitude),
+            zoom: 14.4746
+        ),
+        compassEnabled: true,
+        onMapCreated: (controller) {
+          mapController = controller;
+        },
+        markers: ,
+      );
+    } else{
+      return Scaffold();
+    }
   }
 }
