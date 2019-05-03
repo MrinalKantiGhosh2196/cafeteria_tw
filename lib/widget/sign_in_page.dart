f21e9247e5950b76cb23cb72594667450ebf2917
praveeng <praveeng@INpraveeng.local>
2019-05-03 15:59:20 +0530

    [Mrinal, Praveen ] Add fingerprint unlock

--- lib/widget/sign_in_page.dart
+++ lib/widget/sign_in_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+import 'package:local_auth/local_auth.dart';
 
 class SignInPage extends StatelessWidget{
   @override
@@ -20,6 +21,13 @@ class SignInPage extends StatelessWidget{
               child: FacebookSignInButton(
                   onPressed: () => _signInWithFacebook(context)
               )
+            ),
+            new Padding(padding: EdgeInsets.only(top: 30)),
+            new Padding(
+              padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: MaterialButton(
+                  child: Icon(Icons.fingerprint, size: 200),
+                  onPressed: () => _startAuthentication(context)),
             )
           ],
         ),
@@ -40,6 +48,23 @@ class SignInPage extends StatelessWidget{
    } else{
      Navigator.of(context).pushReplacementNamed('/');
    }
+  }
+
+  _startAuthentication(BuildContext context) async{
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Tapped ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    try{
+      var localAuth = new LocalAuthentication();
+      bool didAuthenticate = await localAuth.authenticateWithBiometrics(
+          localizedReason: 'Please authenticate to show account balance');
+      if(didAuthenticate){
+        Navigator.of(context).pushReplacementNamed("/home");
+      } else{
+        Navigator.of(context).pushReplacementNamed('/');
+      }
+    } catch (e) {
+      print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ${e.toString()} ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+
   }
 
 }
